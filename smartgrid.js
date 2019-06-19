400010b102aaa0db38e34180be7ed46a5e02846d
Dima <no.gandalf.grey@gmail.com>
2019-06-19 11:58:03 +0300

    Initial commit

---
+++ smartgrid.js
@@ -0,0 +1,23 @@
+module.exports = {
+    outputStyle: 'scss',
+    columns: 12,
+    offset: "20px",
+    container: {
+        maxWidth: "1240px",
+        fields: "60px"
+    },
+    breakPoints: {
+        md: {
+            width: "1080px"
+        },
+        sm: {
+            width: "820px"
+        },
+        xs: {
+            width: "640px"
+        },
+        xxs: {
+            width: "420px"
+        }
+    }
+};
